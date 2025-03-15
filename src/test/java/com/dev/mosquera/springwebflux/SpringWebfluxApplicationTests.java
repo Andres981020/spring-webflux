8a4fa40e47a2f0eb23c4e302bac33133f86eb80c
Andres Mosquera <qualitycolombia.dvp09@gmail.com>
2025-03-15 10:59:00 -0500

    First commit for the spring-webflux repo

---
+++ src/test/java/com/dev/mosquera/springwebflux/SpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.mosquera.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
